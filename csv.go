12301bfd9112c2b0fb325d2eaac825c91c6b2568
Calvin Colton <calvincolton@Calvins-MBP-2.hsd1.ca.comcast.net>
2023-11-28 15:14:31 -0800

    more profiling

--- csv.go
+++ csv.go
@@ -25,23 +25,29 @@ func avg(data []float64) float64 {
 
 func csv2float(r io.Reader, column int) ([]float64, error) {
 	cr := csv.NewReader(r)
+	cr.ReuseRecord = true
 
 	// Adjusting for 0 based index as that is what most users are accustomed to
 	column--
 
-	allData, err := cr.ReadAll()
-	if err != nil {
-		return nil, fmt.Errorf("cannot read data from file: %w", err)
-	}
-
 	var data []float64
 
-	for i, row := range allData {
+	for i := 0; ; i++ {
+		row, err := cr.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return nil, fmt.Errorf("cannot read data from file: %w", err)
+		}
+
 		if i == 0 {
 			// skip header row
 			continue
 		}
 
+		// Check number of columns in CSV file
 		if len(row) <= column {
 			// File does not have that many columns
 			return nil, fmt.Errorf("%w: File has only %d columns", ErrInvalidColumn, len(row))
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/calvincolton/go-cli-performance-colstats
 
 go 1.19
+
+require golang.org/x/tools v0.16.0 // indirect
